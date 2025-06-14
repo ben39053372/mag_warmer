f7e747fe4644443d6fbc5f6158398e16a4e5f4e3
BenCheng <ben39053372@gmail.com>
2025-06-14 11:27:10 +0800

    feat: add MCH control

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h> 
 
+// Define Temp DS18B20 pin
 OneWire ds18x20[] = { 1, 2, 3, 4 };
 const int oneWireCount = sizeof(ds18x20) / sizeof(OneWire);
 DallasTemperature sensor[oneWireCount];
-float temp[4];
+
+// Define IRF540N pin to control MCH
+uint MCH1_pin = 10;
+uint MCH2_pin = 20;
+
+int targetTemp = 40;
 
 void setup() {
   // start serial port
@@ -22,6 +28,11 @@ void setup() {
     sensor[i].begin();
     if (sensor[i].getAddress(deviceAddress, 0)) sensor[i].setResolution(deviceAddress, 12);
   }
+
+  // config MCH pin
+  pinMode(MCH1_pin, OUTPUT);
+  pinMode(MCH2_pin, OUTPUT);
+
 }
 
 void loop() {
@@ -35,7 +46,27 @@ void loop() {
     Serial.printf("Temp sensor %d: %f ", i, sensor[i].getTempCByIndex(0));
   }
 
+  checkTemp();
+
   delay(1000);
   Serial.println();
+}
+
+void checkTemp() {
+  if((sensor[0].getTempCByIndex(0) + sensor[1].getTempCByIndex(0)) < targetTemp ) {
+    Serial.println("MCH1 on!");
+    digitalWrite(MCH1_pin, HIGH);
+  } else {
+    Serial.println("MCH1 off!");
+    digitalWrite(MCH1_pin, LOW);
+  }
+
+  if((sensor[2].getTempCByIndex(0) + sensor[3].getTempCByIndex(0)) < targetTemp ) {
+    Serial.println("MCH2 on!");
+    digitalWrite(MCH2_pin, HIGH);
+  } else {
+    Serial.println("MCH2 off!");
+    digitalWrite(MCH2_pin, LOW);
+  }
 }
 
